b15035023da6f3c7dc9671e51e8b01819c93276c
Rami <ralrafie@yahoo.com>
2024-07-27 10:25:28 -0400

    updated not found page and index html

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -1,8 +1,9 @@
+import { Box } from "@mui/material"
 
 export const NotFound = () => {
     return (
-        <>
+        <Box className="error-page-container">
             <p>Error: Page not found.</p>
-        </>
+        </Box>
     )
 }
